39290393ec9371109a8e1134fdc64a27c170bca7
almeida1 <engsoftware1@gmail.com>
2025-06-29 19:42:21 -0300

    versao inicial - conexao com o mongo db

---
+++ assistente3/src/test/java/com/fatec/assistente3/Assistente3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.assistente3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assistente3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
